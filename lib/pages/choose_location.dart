bf956f48444847158919b8a6842ac7ad016d2f66
Karol <kakol.karol00@gmail.com>
2021-02-04 16:53:00 +0100

    final 24

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    print('initState function ran');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build function ran');
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('choose a location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      backgroundColor: Colors.grey[200],
+      body: RaisedButton(
+        onPressed: (){
+          setState(() {
+            counter +=1;
+          });
+        },
+        child: Text('counter is: $counter'),
+      ),
+    );
+  }
+}
